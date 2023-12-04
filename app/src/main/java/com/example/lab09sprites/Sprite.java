524fa096874d53c5df7b37f41e34a33861261542
chetanmaviti <cmaviti93@gmail.com>
2023-12-04 10:52:57 -0500

    second commit

--- app/src/main/java/com/example/lab09sprites/Sprite.java
+++ app/src/main/java/com/example/lab09sprites/Sprite.java
@@ -1,9 +1,37 @@
 package com.example.lab09sprites;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.RectF;
 
 public class Sprite extends RectF {
-    public Sprite(float left, float top, float right, float bottom, int dX, int dY, int Color) {
+    private int dX, dY, color;
+    public Sprite(float left, float top, float right, float bottom, int dX, int dY, int color) {
         super(left, top, right, bottom);
+        this.dX=dX;
+        this.dY=dY;
+        this.color = color;
+    }
+
+    public Sprite(float left, float top, float right, float bottom) {
+        this(left, top, right, bottom,1, 2, Color.MAGENTA);
+    }
+
+    public Sprite(int dX, int dY, int color) {
+        this(1, 1, 11, 11, dX, dY, color);
+    }
+
+    public Sprite() {
+        this(2, 3, Color.GREEN);
+    }
+
+    public void update(){
+        offset(dX, dY);
+    }
+    public void draw(Canvas canvas){
+        Paint paint= new Paint();
+        paint.setColor(color);
+        canvas.drawCircle(centerX(), centerY(), width()/2, paint);
     }
 }
