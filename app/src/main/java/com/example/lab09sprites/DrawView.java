9a44c2871ded980eaf8df7407203735faec58a51
Chetan <cmaviti93@gmail.com>
2023-12-04 10:16:25 -0800

    Initial commit

---
+++ app/src/main/java/com/example/lab09sprites/DrawView.java
@@ -0,0 +1,20 @@
+package com.example.lab09sprites;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class DrawView extends View {
+
+    public DrawView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
---
+++ app/src/main/java/com/example/lab09sprites/Sprite.java
@@ -0,0 +1,9 @@
+package com.example.lab09sprites;
+
+import android.graphics.RectF;
+
+public class Sprite extends RectF {
+    public Sprite(float left, float top, float right, float bottom, int dX, int dY, int Color) {
+        super(left, top, right, bottom);
+    }
+}
